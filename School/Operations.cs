3cc405759385c07f4b48f26a41e12c084940d69f
Foster Stule <camodude5@yahoo.com>
2018-06-05 14:09:33 -0400

    created another employee subclass

---
+++ School/Operations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+	class Operations : Employee
+	{
+
+		public Operations(string name, string email, string phone, string jobtitle, decimal salaray) : base(name, email, phone, jobtitle, salaray)
+		{
+
+		}
+
+		public Operations() {
+
+		}
+	}
+}
